8de650f7722e14e566776c913c0f6ebd23201bf8
kevin palowa <kevinpalowa@gmail.com>
2022-10-05 16:27:29 +0700

    first message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen text-white">
+      <ul>
+        <li>Halo</li>
+        <li>bro</li>
+        <li>an</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
